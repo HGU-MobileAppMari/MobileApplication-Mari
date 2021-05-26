787f97fc443a7187829092b3a67a2ebeb3d18f66
LeeHyunji <leehyunji0715@gmail.com>
2021-05-26 10:57:05 +0900

    add authentication provider and fix login page and home screen page

--- fish_app_mari/lib/screens/login/login.dart
+++ fish_app_mari/lib/screens/login/login.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
-import 'package:provider/provider.dart';
-
-import 'package:fish_app_mari/screens/home/home_screen.dart';
+import 'package:fish_app_mari/auth_service.dart';
+import 'package:fish_app_mari/auth_provider.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final AuthService auth = Provider.of(context).auth;
     return Scaffold(
       body: SafeArea(
         child: ListView(
@@ -34,77 +31,19 @@ class LoginPage extends StatelessWidget {
                   height: 130.0,
                   width: 140.0,
                 ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(40.0, 40.0, 40.0, 5.0),
+                  child: GoogleSignInButton(
+                      centered: true,
+                      onPressed: () {
+                        auth.signInWithGoogle();
+                      }),
+                ),
               ],
-            ),
-            Consumer<ApplicationState>(
-              builder: (context, appState, _) {
-                if (appState.loggedIn) {
-                  print('logged in');
-                  WidgetsBinding.instance.addPostFrameCallback((_) {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => HomeScreen(),
-                        ));
-                  });
-                } else {
-                  print('logged out');
-                }
-                return Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.fromLTRB(40.0, 40.0, 40.0, 5.0),
-                      child: GoogleSignInButton(
-                          centered: true,
-                          onPressed: () {
-                            appState.signInWithGoogle();
-                          }),
-                    ),
-                  ],
-                );
-              },
             ),
           ],
         ),
       ),
     );
-  }
-}
-
-class ApplicationState extends ChangeNotifier {
-  bool loggedIn = false;
-
-  ApplicationState() {
-    init();
-  }
-
-  Future<void> init() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp();
-
-    FirebaseAuth.instance.userChanges().listen((user) {
-      if (user == null) {
-        loggedIn = false;
-        //print('User is signed out!');
-      } else {
-        loggedIn = true;
-        //print('User is signed in!');
-      }
-      notifyListeners();
-    });
-  }
-
-  Future<UserCredential> signInWithGoogle() async {
-    final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
-    final GoogleSignInAuthentication googleAuth =
-        await googleUser.authentication;
-
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-    notifyListeners();
-
-    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
