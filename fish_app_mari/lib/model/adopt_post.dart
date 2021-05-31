a1d6e345489a5438442e0bf7cb789452a9584870
LeeHyunji <leehyunji0715@gmail.com>
2021-05-31 10:32:16 +0900

    change existing bottom navigation bar and add adopt page

---
+++ fish_app_mari/lib/model/adopt_post.dart
@@ -0,0 +1,21 @@
+class AdoptPost {
+  final String userImage;
+  final String userName;
+  final String fishName;
+  final String postImage;
+  final String caption;
+  final String description;
+  final String timestamp;
+  final String location;
+
+  AdoptPost({
+    this.userImage,
+    this.userName,
+    this.fishName,
+    this.postImage,
+    this.caption,
+    this.description,
+    this.timestamp,
+    this.location,
+  });
+}
